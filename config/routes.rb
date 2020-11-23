03058df31919cf93f12c3695e935f3e84c00c28a
David Whitlatch <davidtwhitlatch@gmail.com>
2020-11-23 14:16:30 -0500

    rails init and user resources completed

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users, only: :create
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
