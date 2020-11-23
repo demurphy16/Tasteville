a7338ea847169a2d066368b2e50db84ef8b64ecb
David Whitlatch <davidtwhitlatch@gmail.com>
2020-11-23 14:26:38 -0500

    scaffolded foods and flavors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :flavors
+  resources :foods
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users, only: :create
