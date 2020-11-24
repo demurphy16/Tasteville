d2ac32b385af8555b2ecfaeebfe68c926e556eec
David Whitlatch <davidtwhitlatch@gmail.com>
2020-11-24 12:33:06 -0500

    FE login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
 import './App.css';
 import Layout from './layouts/Layout';
 import Login from './screens/Login';
+import { loginUser } from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
   return (
     <Layout>
       <Switch>
 
         <Route path='/login'>
           {/* login */}
-          <Login />
+          <Login handleLogin={handleLogin} />
         </Route>
 
         <Route path='/register'>
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Login() {
+export default function Login(props) {
   const [formData, setFormData] = useState({
     username: "",
     password: ""
@@ -16,7 +16,10 @@ export default function Login() {
   }
 
   return (
-    <form>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      props.handleLogin(formData);
+    }}>
       <h3>Login</h3>
       <label>Username:
         <input
